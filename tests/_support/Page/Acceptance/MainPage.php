841e05316b813038bdcd5e6d9b03c8a8bce852d8
AiasemKoshimbay <160107038@stu.sdu.edu.kz>
2021-06-15 17:01:53 +0600

    added github actions

---
+++ tests/_support/Page/Acceptance/MainPage.php
@@ -0,0 +1,19 @@
+<?php
+namespace Page\Acceptance;
+namespace Page\Acceptance;
+/**
+ * Класс для главной страницы 
+ */
+class MainPage
+{
+    /**
+     * локатор для подзаголовка
+     */
+    public static $navbarLinks = '#navbar-links';
+
+    /**
+     * селектор категории подзаголовка
+     */
+    public static $categoryLink = '//*[@id="navbar-links"]/li[%d]';
+    
+}
